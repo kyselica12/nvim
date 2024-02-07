f983db0114870df98af0c8651b703f04966aa0c7
kyselica12 <dano.kyselica@gmail.com>
2024-02-07 09:58:35 +0100

    ui messeges removed

--- lua/custom/plugins/ui.lua
+++ lua/custom/plugins/ui.lua
@@ -1,52 +1,52 @@
 return {
-    {
-        "stevearc/dressing.nvim",
-        event = "VeryLazy",
-    },
-    {
-        "stevearc/dressing.nvim",
-        lazy = true,
-        init = function()
-          ---@diagnostic disable-next-line: duplicate-set-field
-          vim.ui.select = function(...)
-            require("lazy").load({ plugins = { "dressing.nvim" } })
-            return vim.ui.select(...)
-          end
-          ---@diagnostic disable-next-line: duplicate-set-field
-          vim.ui.input = function(...)
-            require("lazy").load({ plugins = { "dressing.nvim" } })
-            return vim.ui.input(...)
-          end
-        end,
-    },
-    {
-        "folke/noice.nvim",
-        event = "VeryLazy",
-        opts = {
-        -- add any options here
-        },
-        dependencies = {
-        -- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
-        "MunifTanjim/nui.nvim",
-        -- OPTIONAL:
-        --   `nvim-notify` is only needed, if you want to use the notification view.
-        --   If not available, we use `mini` as the fallback
-        "rcarriga/nvim-notify",
-        },
-        opts = {
-          lsp = {
-            override = {
-              ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
-              ["vim.lsp.util.stylize_markdown"] = true,
-              ["cmp.entry.get_documentation"] = true,
-            },
-          },
-          presets = {
-            bottom_search = true,
-            command_palette = true,
-            long_message_to_split = true,
-            inc_rename = true,
-          },
-        },
-    },
+    -- {
+    --     "stevearc/dressing.nvim",
+    --     event = "VeryLazy",
+    -- },
+    -- {
+    --     "stevearc/dressing.nvim",
+    --     lazy = true,
+    --     init = function()
+    --       ---@diagnostic disable-next-line: duplicate-set-field
+    --       vim.ui.select = function(...)
+    --         require("lazy").load({ plugins = { "dressing.nvim" } })
+    --         return vim.ui.select(...)
+    --       end
+    --       ---@diagnostic disable-next-line: duplicate-set-field
+    --       vim.ui.input = function(...)
+    --         require("lazy").load({ plugins = { "dressing.nvim" } })
+    --         return vim.ui.input(...)
+    --       end
+    --     end,
+    -- },
+    -- {
+    --     "folke/noice.nvim",
+    --     event = "VeryLazy",
+    --     opts = {
+    --     -- add any options here
+    --     },
+    --     dependencies = {
+    --     -- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
+    --     "MunifTanjim/nui.nvim",
+    --     -- OPTIONAL:
+    --     --   `nvim-notify` is only needed, if you want to use the notification view.
+    --     --   If not available, we use `mini` as the fallback
+    --     "rcarriga/nvim-notify",
+    --     },
+    --     opts = {
+    --       lsp = {
+    --         override = {
+    --           ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+    --           ["vim.lsp.util.stylize_markdown"] = true,
+    --           ["cmp.entry.get_documentation"] = true,
+    --         },
+    --       },
+    --       presets = {
+    --         bottom_search = true,
+    --         command_palette = true,
+    --         long_message_to_split = true,
+    --         inc_rename = true,
+    --       },
+    --     },
+    -- },
 }

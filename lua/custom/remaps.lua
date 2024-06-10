6a0b3d0c5287f00b81511761fd39b2c2ccc99a7e
kyselica12 <dano.kyselica@gmail.com>
2024-06-10 09:55:14 +0200

    Remap tabNext

--- lua/custom/remaps.lua
+++ lua/custom/remaps.lua
@@ -5,12 +5,11 @@ vim.keymap.set('n', '<Leader>q', '<cmd>q<cr>')
 
 vim.keymap.set('n', 'Q', '@q')
 
-vim.keymap.set('n', '<Tab>', '<cmd>tabnext<cr>')
+
 vim.keymap.set('n', '<C-H>', '<C-W>h')
 vim.keymap.set('n', '<C-J>', '<C-W>j')
 vim.keymap.set('n', '<C-K>', '<C-W>k')
 vim.keymap.set('n', '<C-L>', '<C-W>l')
-
 vim.keymap.set('n', '<C-D>', '<C-D>zz')
 vim.keymap.set('n', '<C-U>', '<C-U>zz')
 
@@ -27,4 +26,8 @@ vim.keymap.set("n", "<leader>sj", "<cmd>sv<cr>")
 vim.keymap.set("n", "<leader>sl", "<cmd>vsplit<cr>")
 vim.keymap.set("n", "<leader>t", "<cmd>tabnew<cr>")
 
+-- vim.keymap.set('n', '<Tab>', '<cmd>tabnext<cr>')
+vim.keymap.set("n", "<Tab>", "<cmd>bNext<cr>")
+vim.keymap.set("n", "<S-Tab>", "<cmd>bprev<cr>")
+-- vim.keymap.set("n", "<Shift><Tab>", "<cmd>bNext<cr>")
 
